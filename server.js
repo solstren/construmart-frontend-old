e101e4915a3791749c21788d48799ff718c60d8d
Gabriel Ifeanyi <gabriel_ifeanyi@ymail.com>
2019-11-20 06:16:19 +0100

    made some changes. hope eit works

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/construmart-admin'));
+app.use(express.static('./dist/construmart-frontend'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/construmart-admin/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/construmart-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
